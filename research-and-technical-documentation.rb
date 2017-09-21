056867a4eab7b2377f2a65cd97e6761961e02508
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-21 15:14:55 -0400

    Define length

---
+++ research-and-technical-documentation.rb
@@ -0,0 +1,8 @@
+#String
+#length
+#Returns the number of elements within an array, string, or collection
+greetings = ["hi", "hello", "hiya"]
+greetings.length
+
+polite_greeting = "Hi there, very nice to meet you."
+polite_greeting.lenth
