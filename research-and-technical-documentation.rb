bbcd55e1ff1aae2add11168b6d81682e689d0a9d
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-21 15:23:01 -0400

    Define strip

--- research-and-technical-documentation.rb
+++ research-and-technical-documentation.rb
@@ -1,8 +1,16 @@
-#String
-#length
+#STRING
+puts "length"
 #Returns the number of elements within an array, string, or collection
 greetings = ["hi", "hello", "hiya"]
-greetings.length
+puts greetings
+puts greetings.length
 
 polite_greeting = "Hi there, very nice to meet you."
-polite_greeting.lenth
+puts polite_greeting
+puts polite_greeting.length
+
+puts "strip"
+#Returns a copy of the string with whitespace at the beginning and end removed
+long_polite_greeting = "      Hi there, very nice to meet you.       "
+puts long_polite_greeting
+puts polite_greeting.strip
